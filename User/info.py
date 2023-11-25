1fdbf1d582e24b2beb7045dcfd985bd40edb05c3
ManojKumar <manojkumarkalyanapu777@gmail.com>
2023-11-25 11:31:11 +0530

    first commit

--- User/info.py
+++ User/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'manojkumarkalyanapu777@gmail.com'
-EMAIL_HOST_PASSWORD = 'lrlrinliaqbjteav'
+EMAIL_HOST_USER = 'your Mail'
+EMAIL_HOST_PASSWORD = 'System generate Password'
 EMAIL_PORT = 587
